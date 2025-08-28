cd047e8bee92353bdf715ad556def7fe010f5807
Jeshuan29 <jetorresgo@est.utn.ac.cr>
2025-08-28 01:28:46 -0600

    Enum Tipo Licencia
    Enum para los tipos de licencia

---
+++ src/Gestion_Personas/Tipo_Licencia.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Gestion_Personas;
+
+/**
+ *
+ * @author Jeshuan
+ */
+public enum Tipo_Licencia {
+    B1("Tipo B1"),
+    B2("Tipo B2"),
+    B3("Tipo B3");
+    private String licencia;
+
+    private Tipo_Licencia(String licencia) {
+        this.licencia = licencia;
+    }
+     
+}
