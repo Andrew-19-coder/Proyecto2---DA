d9c959dd7a53e108fc326992ea612e491d149084
Ninjasuperestudioso <jocamposra@est.utn.ac.cr>
2025-08-28 02:06:38 -0600

    Excepciones
    Se crea la Excepcion tieneReservasActivas para validadr que no se pueda borrar un cliente con reservas activas

---
+++ src/Exceptions/ReservaActivaException.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exceptions;
+
+/**
+ *
+ * @author Joan
+ */
+public class ReservaActivaException extends Exception {
+   public ReservaActivaException() {
+        super("No se puede eliminar el cliente: tiene reservas activas");
+    }
+
+    public ReservaActivaException(String nombreCliente) {
+        super("No se puede eliminar el cliente " + nombreCliente + ": tiene reservas activas");
+    }  
+}
